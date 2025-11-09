d9fff7d2fff231158744811136a8616b6f5ebc3c
harrietfiahagbe-dev <harrietfiahagbe@gmail.com>
2025-11-09 00:47:15 +0000

    hello

---
+++ src/TalkEvents.java
@@ -0,0 +1,15 @@
+import java.time.LocalDateTime;
+
+public class TalkEvents {
+    private String nameEvents;
+    private String location;
+    private LocalDateTime date;
+    private LocalDateTime time;
+    private int duration;
+    private String speakerName;
+    private String speakerProfession;
+    private String talkAbstract;
+    private int maxQuestions;
+}
+
+
